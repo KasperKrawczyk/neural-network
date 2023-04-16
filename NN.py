ceec5b6ef5734fcf0842859dc3aa0b31599d5110
KasperKrawczyk <kasper.krawczyk@gmail.com>
2023-04-16 12:09:30 +0100

    bk_prop

--- NN.py
+++ NN.py
@@ -20,13 +20,22 @@ class Network:
                 
     def run(self):
         self.cur_out = self.fwd_prop()
-        self.cur_err = self.get_error()
+        self.bk_prop()
 
     def fwd_prop(self):
         cur_out = self.input
         for layer in self.network:
             cur_out = layer.fwd_prop(cur_out)
         return cur_out
+
+    def bk_prop(self, expected: np.ndarray):
+        for i in reversed(range(len(self.network))):
+            layer = self.network[i]
+            if i != len(self.network) - 1:
+                prev_layer_err = self.network[i + 1].get_cur_err()
+                layer.cur_delta = prev_layer_err * transfer_derivative(layer.cur_out)
+            else:
+                layer.cur_delta = layer.cur_out - expected * transfer_derivative(layer.cur_out)
 
     def get_error(self, y: np.ndarray):
         (self.cur_out - y) * transfer_derivative()
@@ -43,19 +52,27 @@ class Layer:
     def __init__(self, num_neurons: int, prev_layer_num_neurons: int):
         self.num_neurons = num_neurons
         self.prev_layer_num_neurons = prev_layer_num_neurons
-        self.mat = np.random.rand(num_neurons, prev_layer_num_neurons + 1)
-        self.dim = self.mat.shape
+        self.w = np.random.rand(num_neurons, prev_layer_num_neurons + 1)
+        self.cur_out: np.ndarray = None
+        self.cur_delta: np.ndarray = None
+        self.cur_err: np.ndarray = None
+        self.dim = self.w.shape
         self._vec_transfer = np.vectorize(transfer)
 
+    def get_cur_err(self):
+        self.cur_err = np.dot(self.w, self.cur_delta)
+        return self.cur_err
+
     def _activate(self, ins: np.ndarray):
-        return (np.dot(self.mat[:, :-1], ins) + self.mat[:, -1:].T)[0]
+        return (np.dot(self.w[:, :-1], ins) + self.w[:, -1:].T)[0]
 
     def _transfer(self, mat: np.ndarray):
         return self._vec_transfer(mat)
 
     def fwd_prop(self, ins: np.ndarray):
         activation = self._activate(ins)
-        return self._transfer(activation)
+        self.cur_out = self._transfer(activation)
+        return self.cur_out
 
 if __name__ == '__main__':
     n = Network(dimensions=[3, 5, 5, 2])
